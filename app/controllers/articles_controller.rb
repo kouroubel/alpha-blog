707989d5c4ef58d1a7df45105646d014b714e8ad
Ruby on Rails <kouroubel@gmail.com>
2018-01-09 22:39:20 +0000

    Fix create article, ad flash and error msgs, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,28 @@ class ArticlesController < ApplicationController
 
 
   def create
-    #render plain: params[:article].inspect
     
-    @article = Article.new(article_params)
-    @article.save
+    #The browser sends information (model variable values) to the controller 
+    #through the params hash 
     
-    redirect_to articles_show(@article)
+    #render plain: params[:article].inspect //return the contents of the hash as a string
+    
+    #need to whitelist the information passed from the browser before saving
+    #article_params method will do that
+    @article = Article.new(article_params) # this instance variables has the whitelisted (permitted) values
+    
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+      
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+    
   end
   
   private 
